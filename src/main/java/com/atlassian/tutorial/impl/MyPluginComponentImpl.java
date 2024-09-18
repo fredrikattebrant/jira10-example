7c0628f9b49c7b4d96dba30d356f4711ea3912b6
Fredrik Attebrant <fredrik.attebrant@gmail.com>
2024-09-18 12:32:34 +0200

    First commit
    New plugin
    + web-section
    + web-item

---
+++ src/main/java/com/atlassian/tutorial/impl/MyPluginComponentImpl.java
@@ -0,0 +1,23 @@
+package com.atlassian.tutorial.impl;
+
+import com.atlassian.sal.api.ApplicationProperties;
+import com.atlassian.tutorial.api.MyPluginComponent;
+
+
+public class MyPluginComponentImpl implements MyPluginComponent
+{
+        private final ApplicationProperties applicationProperties;
+
+    
+    public MyPluginComponentImpl(final ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
+
+    public String getName() {
+        if (null != applicationProperties) {
+            return "myComponent:" + applicationProperties.getDisplayName();
+        }
+
+        return "myComponent";
+    }
+}
